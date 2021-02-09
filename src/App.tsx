54ed5a64a5a1a2d8acd62efa7df5a818331d5a1c
lucas lopes <lopes@arquia.com>
2021-02-09 19:45:37 -0300

    add fonts, page signin and signup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View, StatusBar } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
 
@@ -10,7 +10,7 @@ const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" />
     <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-      <Routes/>
+      <Routes />
     </View>
   </NavigationContainer>
 );
